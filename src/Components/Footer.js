75a1a2f4594158067a479bf8a1d0455caabb05cf
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-23 01:59:18 +0300

    Added Footer+CSS

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,42 @@
 import React from "react";
+import "../styling/FooterStyling/Footer.css";
 
 export default function Footer(){
     return (
-        <footer>
-            <div id="homepage-footer">
-                <h1>Hi I'm footer</h1>
+       <footer className="footer">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3 className="title" >Services</h3>
+                        <ul>
+                            <li><a href="#">Dog Breeds</a></li>
+                            <li><a href="#">Subscription</a></li>
+                            <li><a href="#">Download</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3 className="title">About Us</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3 className="title">Security</h3>
+                        <ul>
+                            <li><a href="#">Terms of service</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Legalities</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 item social">
+                        <a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a>
+                        <a href="#"><i class="icon ion-social-snapchat"></i></a>
+                        <a href="#"><i class="icon ion-social-instagram"></i></a>
+                        <p class="copyright">HotDog © 2023</p>
+                    </div>
+                </div>
             </div>
         </footer>
     )
