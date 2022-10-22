dd1aa689f7d0112d46c27d0eea928b5f4bfc0e9a
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-22 12:11:54 +0300

    added missing Data and link

--- src/Components/BodyComponents/DogCardList.js
+++ src/Components/BodyComponents/DogCardList.js
@@ -1,17 +1,18 @@
 import Card from "./Card";
 import "../Data/DogBreedData.json";
 import "../../styling/BodyStyling/DogCardList.css";
-import DogBreedData from "../Data/DogBreedData.json";
+import DogBreedData from "../../Data/DogBreedData.json";
 export default function Body(){
     const dogCards = DogBreedData.map(dogBreed => {return(
         <Card
          key={dogBreed.id} 
-         dogBreed={dogBreed}
+         {...dogBreed}
          />
         )
     })
     return (
             <section className="cards-list">
+                <input type="text" placeholder="Search for a dog breed" className="search-bar"/>
                 {dogCards}
             </section>
     )
