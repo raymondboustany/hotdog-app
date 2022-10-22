e3db747e8146fb9bf47c9997008f68c96956e703
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-22 12:15:09 +0300

    fixed propagation of props

--- src/Components/BodyComponents/Card.js
+++ src/Components/BodyComponents/Card.js
@@ -3,9 +3,9 @@ import "../../styling/BodyStyling/Card.css";
 export default function Card(props){
     return (
         <div className="card">
-                <img src={props.dogBreed.img} className="card-img"/>
+                <img src={props.img} className="card-img"/>
                 <div className="name-container">
-                    <h1 className="card-name">{props.dogBreed.name}</h1>
+                    <h1 className="card-name">{props.name}</h1>
                 </div>
                 
         </div>
