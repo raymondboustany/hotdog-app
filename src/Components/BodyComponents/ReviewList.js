6d8ef70421e813b3680969a79ee96af39bc8ec39
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-24 03:53:20 +0300

    Lot's of small changes

--- src/Components/BodyComponents/ReviewList.js
+++ src/Components/BodyComponents/ReviewList.js
@@ -1,8 +1,9 @@
 import "../../styling/BodyStyling/ReviewList.css";
 import Review from "./Review";
 import ReviewData from "../../Data/ReviewsData.json";
-
-export default function ReviewList(props){
+var slideIndex = 0;
+carousel();
+export default function ReviewList(){
     const reviews =ReviewData.map(review => {return(
         <Review 
             key={review.id} 
@@ -17,4 +18,16 @@ export default function ReviewList(props){
             </div>
         </div>
     )
+}
+export function carousel() {
+  var i;
+  var x = document.getElementsByClassName("review-list-container");
+  console.log(x);
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  (x[slideIndex-1]).display = "block";
+  setTimeout(carousel, 10000); // Change image every 10 seconds
 }
