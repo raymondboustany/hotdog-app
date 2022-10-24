8e79f34354f2c736f783b723bc54f5e8da5c8688
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-24 03:53:47 +0300

    Lots of changes

--- src/Components/BodyComponents/Review.js
+++ src/Components/BodyComponents/Review.js
@@ -1,11 +1,14 @@
 import "../../styling/BodyStyling/Review.css";
-// import reviewImg from "../../images/DowPawHeart.png";
 export default function Review(props){
     return (
         <div className="review">
             <div className="review-container">
                 <h1 className="review-name">{props.profile.name}</h1>
-                <p className="review-text">{props.testimony.text}</p>
+                <div id="heart-wrapper">
+                    <div id="heart-container">
+                        <p className="review-text">{props.testimony.text}</p>
+                    </div>
+                </div>
             </div>
             <img src={props.profile.picture} className="review-img"/>
         </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,9 @@ import "../styling/FooterStyling/Footer.css";
 export default function Footer(){
     return (
        <footer className="footer">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-sm-4 col-md-3 item">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-sm-4 col-md-3 item">
                         <h3 className="title" >Services</h3>
                         <ul>
                             <li><a href="#">Dog Breeds</a></li>
@@ -14,7 +14,7 @@ export default function Footer(){
                             <li><a href="#">Download</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-4 col-md-3 item">
+                    <div className="col-sm-4 col-md-3 item">
                         <h3 className="title">About Us</h3>
                         <ul>
                             <li><a href="#">Company</a></li>
@@ -22,7 +22,7 @@ export default function Footer(){
                             <li><a href="#">Contact Us</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-4 col-md-3 item">
+                    <div className="col-sm-4 col-md-3 item">
                         <h3 className="title">Security</h3>
                         <ul>
                             <li><a href="#">Terms of service</a></li>
@@ -30,11 +30,11 @@ export default function Footer(){
                             <li><a href="#">Legalities</a></li>
                         </ul>
                     </div>
-                    <div class="col-lg-3 item social">
-                        <a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a>
-                        <a href="#"><i class="icon ion-social-snapchat"></i></a>
-                        <a href="#"><i class="icon ion-social-instagram"></i></a>
-                        <p class="copyright">HotDog © 2023</p>
+                    <div className="col-lg-3 item social">
+                        <a href="#"><i className="icon ion-social-facebook"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a>
+                        <a href="#"><i className="icon ion-social-snapchat"></i></a>
+                        <a href="#"><i className="icon ion-social-instagram"></i></a>
+                        <p className="copyright">HotDog © 2023</p>
                     </div>
                 </div>
             </div>
