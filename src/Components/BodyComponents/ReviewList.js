f6ede644b7ce048c91d99c6ba5855927d1ac1ae6
Rami Kassouf <Rami.kassouf@net.usj.edu.lb>
2022-10-24 16:54:06 +0300

    fixed darkmode position

--- src/Components/BodyComponents/ReviewList.js
+++ src/Components/BodyComponents/ReviewList.js
@@ -1,8 +1,7 @@
 import "../../styling/BodyStyling/ReviewList.css";
 import Review from "./Review";
 import ReviewData from "../../Data/ReviewsData.json";
-// var slideIndex = 0;
-// carousel();
+var slideIndex = 0;
 export default function ReviewList(){
     const reviews =ReviewData.map(review => {return(
         <Review 
@@ -19,15 +18,15 @@ export default function ReviewList(){
         </div>
     )
 }
-// export function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("review-list-container");
-//   console.log(x);
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].display = "block";
-//   setTimeout(carousel, 10000); // Change image every 10 seconds
-// }
+export function carousel() {
+  var i;
+  var x = document.getElementsByClassName("review-list-container");
+  console.log(x);
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 10000); // Change image every 10 seconds
+}
